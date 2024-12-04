72d12260d582b53ece6889ad6035d4efb912162e
harisjumailsilverstream <jumail@silverstream.my>
2024-12-04 22:47:09 +0700

    xxx

--- app/components/ProductCarousel.tsx
+++ app/components/ProductCarousel.tsx
@@ -5,6 +5,10 @@ import React, { useState } from "react";
 const ProductCarousel = ({ images }: { images: string[] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  if (images.length === 0) {
+    return <p>No images available</p>;
+  }
+
   const nextImage = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
@@ -13,19 +17,17 @@ const ProductCarousel = ({ images }: { images: string[] }) => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
-  if (images.length === 0) {
-    return <p>No images available</p>;
-  }
-
   return (
     <div>
-      <button onClick={prevImage}>Previous</button>
-      <img
+      {/* Show only one image */}
+      {/* <img
         src={images[currentIndex] || "/placeholder.png"}
         alt={`Image ${currentIndex + 1}`}
-        width="100"
-      />
-      <button onClick={nextImage}>Next</button>
+        width="300"
+        height="200"
+      /> */}
+      
+      {/* Optional: buttons to navigate between images */}
     </div>
   );
 };
